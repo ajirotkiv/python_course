875afd984b39a4bde1c0d3162c1fb43cef9e584e
Natalija Konovalova <natalja.konovalova@gmail.com>
2025-01-28 18:35:26 +0200

    created moduliai file

---
+++ Moduliai_importai.py
@@ -0,0 +1,10 @@
+# import random
+# atsitiktinis_skaicius = random.randint(1,10)
+# print(f'Atsitiktinis skaicius:{atsitiktinis_skaicius}')
+
+import random
+atsitiktinis_randint = random.randint(1, 100)
+print(f'Atsitiktinis skaicius:{atsitiktinis_randint}')
+
+atsitiktinis_uniform = random.uniform(1, 50)
+print(f'Atsitiktinis skaicius:{atsitiktinis_uniform}')
