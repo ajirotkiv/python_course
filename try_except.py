428d9eb75c2ef09d9c2656fde2539ab2b3e3c741
Natalija Konovalova <natalja.konovalova@gmail.com>
2025-01-30 20:45:24 +0200

    try except practice

--- try_except.py
+++ try_except.py
@@ -10,16 +10,31 @@
 #
 # dalinti = lambda a, b: a/5
 
+#
+# num1 = input('Iveskite pirma skaiciu: ')
+# num2 = input('Iveskite antra skaiciu: ')
+# try:
+#     num1 = int(num1)
+#     num2 = int(num2)
+#     res =  num1 / num2
+#     print(f'Resultatas: {res}')
+# except ValueError:
+#     print('Turi buti iveskas skaicius!')
+#
+# except ZeroDivisionError:
+#     print('Dalyba is nulio negalima. Iveskite kita skaiciu')
 
-num1 = input('Iveskite pirma skaiciu: ')
-num2 = input('Iveskite antra skaiciu: ')
+
+num = input("Iveskite skaiciu: ")
+
 try:
-    num1 = int(num1)
-    num2 = int(num2)
-    res =  num1 / num2
-    print(res)
-except ValueError:
-    print('Turi buti iveskas skaicius!')
+    number = int(num)
 
-except ZeroDivisionError:
-    print('Dalyba is nulio negalima. Iveskite kita skaiciu')
+except ValueError:
+    print('Konvertacija nesekminga, bandykite dar karta')
+
+else:
+    print(f'Konversija sekminga {number}')
+
+finally:
+    print('Programa baige darba')
