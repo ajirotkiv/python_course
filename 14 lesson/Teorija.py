44996e0aa291be9a6db594ea43b9783a17406e93
Natalija Konovalova <natalja.konovalova@gmail.com>
2025-02-04 22:06:33 +0200

    Objektinis programavimas

--- 14 lesson/Teorija.py
+++ 14 lesson/Teorija.py
@@ -1,3 +1,6 @@
+from Tools.scripts.verify_ensurepip_wheels import print_error
+
+
 class House:
     country = 'LT'
     def __init__(self, price, year):
@@ -34,3 +37,47 @@ print(age)
 house_instance_2 = House(5_000_000, 2001)
 age = house_instance_2.get_age()
 print(age)
+
+# ===============
+class House:
+    country = 'LT'
+
+    def __init__(self, price, year):
+        self.price = price
+        self.year = year
+
+    def __str__(self):
+        return f'Namas {self.year} pastatymo, kaina - {self.price}, amzius - {self.get_age()}'
+
+    def get_age(self):
+        return datetime.today().year -self.year
+
+# book1 = house(190000, 2024)
+# print(book1)
+#
+# houses = [
+#     house(199000, 2025),
+#     house(65000, 2008),
+#     house(199000, 2025)
+# ]
+# for house in houses:
+#     print(house)
+#     print(f'Sena kaina: {house.price}')
+#     house.price = round(house.price * 1.22)
+houses_kaupiklis = []
+while True:
+    # quit_choice = input('Iveskite Exit, kad iseiti. Spaskite Enter, kad testi.')
+    # if quit_choice == 'exit':
+    #     break
+
+    year = int(input('Iveskite metus: '))
+    price = int(input('Iveskite kaina: '))
+
+    house_instance = House(price, year)
+    houses_kaupiklis.append(house_instance)
+
+    print('Spausdinam ka sukaupem:')
+    for house in houses_kaupiklis:
+        print(house)
+
+
